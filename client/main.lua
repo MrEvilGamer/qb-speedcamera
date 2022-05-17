0eaf79ae4cba66715aba608f275e68a80da51006
Aditya Nath <84228628+MrEvilGamer@users.noreply.github.com>
2022-05-18 01:23:05 +0530

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -8,8 +8,6 @@ Config.Speedcamera80Zone = {}
 
 Config.Speedcamera120Zone = {}
 
-Config.Blips = {}
-
 -- BELOVE IS YOUR SETTINGS, CHANGE THEM TO WHATEVER YOU'D LIKE & MORE SETTINGS WILL COME IN THE FUTURE! --
 local useBilling = true -- OPTIONS: (true/false)
 local useCameraSound = true -- OPTIONS: (true/false)
@@ -36,8 +34,23 @@ function hintToDisplay(text)
 	DisplayHelpTextFromStringLabel(0, 0, 1, -1)
 end
 
+local blips = {
+	-- 60KM/H ZONES
+	{title="Speedcamera (60KM/H)", colour=1, id=1, x = -524.2645, y = -1776.3569, z = 21.3384}, -- 60KM/H ZONE
+	
+	-- 80KM/H ZONES
+	{title="Speedcamera (80KM/H)", colour=1, id=1, x = 2506.0671, y = 4145.2431, z = 38.1054}, -- 80KM/H ZONE
+	{title="Speedcamera (80KM/H)", colour=1, id=1, x = 1258.2006, y = 789.4199, z = 104.2190}, -- 80KM/H ZONE
+	{title="Speedcamera (80KM/H)", colour=1, id=1, x = 980.9982, y = 407.4164, z = 92.2374}, -- 80KM/H ZONE
+	
+	-- 120KM/H ZONES
+	{title="Speedcamera (120KM/H)", colour=1, id=1, x = 1584.9281, y = -993.4557, z = 59.3923}, -- 120KM/H ZONE
+	{title="Speedcamera (120KM/H)", colour=1, id=1, x = 2442.2006, y = -134.6004, z = 88.7765}, -- 120KM/H ZONE
+	{title="Speedcamera (120KM/H)", colour=1, id=1, x = 2871.7951, y = 3540.5795, z = 53.0930} -- 120KM/H ZONE
+}
+
 CreateThread(function()
-	for _, info in pairs(Config.Blips) do
+	for _, info in pairs(blips) do
 		if useBlips == true then
 			info.blip = AddBlipForCoord(info.x, info.y, info.z)
 			SetBlipSprite(info.blip, info.id)
